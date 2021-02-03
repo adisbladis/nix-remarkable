e5b7155776424ded48626741ace8abf9cc8e46d7
adisbladis <adisbladis@gmail.com>
2021-02-03 18:57:32 +0100

    kernel_headers: Use fetchurl instead of fetchFromGitHub
    fetchFromGitHub uses fetchzip under the hood which breaks bootstrapping

--- pkgs/linux-remarkable/src.nix
+++ pkgs/linux-remarkable/src.nix
@@ -1,11 +1,11 @@
-{ fetchFromGitHub, }:
+{ fetchurl }:
 
-{
+let
+  rev = "1774e2a6a091fdc081324e966d3db0aa9df75c0b";
+in {
   version = "4.9.84-zero-gravitas";
-  src = fetchFromGitHub {
-    owner = "reMarkable";
-    repo = "linux";
-    rev = "1774e2a6a091fdc081324e966d3db0aa9df75c0b";
-    sha256 = "0pjk6ag2s685ar6d31sx484k1wyhyn7g7mz9zib910zcmfb3rdqf";
+  src = fetchurl {
+    url = "https://github.com/reMarkable/linux/archive/${rev}.tar.gz";
+    sha256 = "103qsl5dma6i680bf5bsc6n4fk684k03df4nlzjjmmwx8pz7qnqb";
   };
 }
